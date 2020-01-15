8624874e9f3821ca29d1659f03c67ccd6d45b0e4
kimseongyu <kskksk0925@naver.com>
2020-01-15 19:16:07 +0900

    solve_4673

---
+++ Algorithm_java/src/Backjoon/Baekjoon4673.java
@@ -0,0 +1,26 @@
+package Backjoon;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Baekjoon4673 {
+	public static void main(String[] args) {
+		List<Integer> arr = new LinkedList<>();
+		
+		for(int i = 1; i <= 10000;i++) {
+			arr.add(i);
+		}
+		for(int i = 1; i <= 10000;i++) {
+			int selfnum = i;
+			for(int j = 0; j < (int)(Math.log10(i)+1); j++) {
+				selfnum += (i%((int)(Math.pow(10, j+1))))/((int)(Math.pow(10, j)));
+			}
+			if(arr.indexOf(selfnum)>0) {
+				arr.remove(arr.indexOf(selfnum));
+			}
+		}
+		for(int i = 0; i < arr.size(); i++) {
+			System.out.println(arr.get(i));
+		}		
+	}
+}
