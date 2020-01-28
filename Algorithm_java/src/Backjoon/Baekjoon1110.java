0d2e822479dd324c2198b40ef7254fbff488a441
kimseongyu <kskksk0925@naver.com>
2020-01-28 18:42:01 +0900

    solve

---
+++ Algorithm_java/src/Backjoon/Baekjoon1110.java
@@ -0,0 +1,18 @@
+package Backjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon1110 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int firstNum = input.nextInt();
+		int nextNum = firstNum;
+		int cycle = 0;
+		do {
+			nextNum = nextNum%10*10 + (nextNum/10 + nextNum%10)%10;
+			cycle++;
+		}while(firstNum != nextNum);
+		System.out.println(cycle);
+	}
+}
