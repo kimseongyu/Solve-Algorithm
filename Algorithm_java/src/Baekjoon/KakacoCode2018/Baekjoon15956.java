22bb11f6f0811f11ed465df54f290825fb3bb689
kimseongyu <kskksk0925@naver.com>
2020-02-03 21:41:07 +0900

    solve

---
+++ Algorithm_java/src/Baekjoon/KakacoCode2018/Baekjoon15956.java
@@ -0,0 +1,11 @@
+package Baekjoon.KakacoCode2018;
+
+import java.util.Scanner;
+
+public class Baekjoon15956 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String code = input.next();
+		String[] eachCode = code.split("&&");
+	}
+}
