763b11a2575ee01f49f0836f31f3647331a0e47f
kimseongyu <kskksk0925@naver.com>
2020-02-12 17:54:26 +0900

    solve

---
+++ Algorithm_java/src/Baekjoon/Disjointset/Baekjoon1976.java
@@ -0,0 +1,51 @@
+package Baekjoon.Disjointset;
+
+import java.util.Scanner;
+
+public class Baekjoon1976 {
+	static int[] root;
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int N = input.nextInt();
+		int M = input.nextInt();
+		root = new int[N];
+		for(int i = 0; i < N; i++)
+		    root[i] = i;
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				int a = input.nextInt();
+				if(a == 1) {
+					Union(i,j);
+				}
+			}
+		}
+		int a = Find(input.nextInt()-1);
+		int b = 0;
+		for(int i = 1; i < M; i++) {
+			b = Find(input.nextInt()-1);
+			if(a != b) {
+				System.out.print("NO");
+				break;
+			}
+		}
+		if(a == b) {
+			System.out.print("YES");
+		}
+	}
+	
+	static int Find(int x){
+	    if(root[x] == x){
+	        return x;
+	    } else {
+	        return root[x] = Find(root[x]);
+	    }
+	}
+	
+	static void Union(int x, int y){
+	    x = Find(x);
+	    y = Find(y);
+	    root[y] = x;
+	}
+}
--- Algorithm_java/src/Baekjoon/KakacoCode2018/Baekjoon15956.java
+++ Algorithm_java/src/Baekjoon/KakacoCode2018/Baekjoon15956.java
@@ -1,5 +1,7 @@
 package Baekjoon.KakacoCode2018;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Baekjoon15956 {
@@ -7,5 +9,16 @@ public class Baekjoon15956 {
 		Scanner input = new Scanner(System.in);
 		String code = input.next();
 		String[] eachCode = code.split("&&");
+		Map<String, String> equal = new HashMap<String, String>();
+		Map<String, String> nonEqual = new HashMap<String, String>();
+		for(int i = 0; i < eachCode.length; i++) {
+			if(eachCode[i].contains("==")) {
+				String[] e = eachCode[i].split("==");
+				equal.put(e[0], e[1]);
+			}else if(eachCode[i].contains("!=")) {
+				String[] ne = eachCode[i].split("!=");
+				equal.put(ne[0], ne[1]);
+			}
+		}
 	}
 }
