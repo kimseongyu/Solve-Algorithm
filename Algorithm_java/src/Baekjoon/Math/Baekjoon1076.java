e47325ee5652e6046fecaa9a3e6b7bfa7f6beeff
kimseongyu <kskksk0925@naver.com>
2020-02-27 19:48:23 +0900

    solve 1107

---
+++ Algorithm_java/src/Baekjoon/Math/Baekjoon1076.java
@@ -0,0 +1,25 @@
+package Baekjoon.Math;
+
+import java.util.Scanner;
+
+public class Baekjoon1076 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		String[] a = {"black","brown","red","orange","yellow","green","blue","violet","grey","white"};
+		long v = 0;
+		String b = s.next();
+		for(int i = 0; i < 10; i++)
+			if(a[i].equals(b))
+				v += i*10;
+		b = s.next();
+		for(int i = 0; i < 10; i++)
+			if(a[i].equals(b))
+				v += i;
+		b = s.next();
+		for(int i = 0; i < 10; i++)
+			if(a[i].equals(b))
+				v *= Math.pow(10, i);
+		System.out.print(v);
+	}
+}
