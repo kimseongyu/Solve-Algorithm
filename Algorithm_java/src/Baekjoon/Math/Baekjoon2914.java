7912dc71f2825ea24f0ee447b3ee74d4bf05eb65
kimseongyu <kskksk0925@naver.com>
2020-02-28 21:59:28 +0900

    Create Baekjoon2914.java

---
+++ Algorithm_java/src/Baekjoon/Math/Baekjoon2914.java
@@ -0,0 +1,10 @@
+package Baekjoon.Math;
+
+import java.util.Scanner;
+
+public class Baekjoon2914 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print(s.nextInt()*(s.nextInt()-1)+1);
+	}
+}
