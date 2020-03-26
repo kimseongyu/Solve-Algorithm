5230cb8b1cb23992e6ada0c5b7bfbe22b20f5063
kimseongyu <kskksk0925@naver.com>
2020-03-26 22:29:04 +0900

    solve

---
+++ Algorithm_java/src/Baekjoon/Math/Baekjoon1629.java
@@ -0,0 +1,10 @@
+package Baekjoon.Math;
+
+import java.util.Scanner;
+
+public class Baekjoon1629 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print(Math.pow(10, 11)%);
+	}
+}
